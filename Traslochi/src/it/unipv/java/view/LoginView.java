e4f026329336a592d0838b6d0ccddf5926cf0ba5
NikoManev <nikola.manev01@universitadipavia.it>
2024-02-07 23:21:28 +0100

    Perfezionamento LoginView

--- Traslochi/src/it/unipv/java/view/LoginView.java
+++ Traslochi/src/it/unipv/java/view/LoginView.java
@@ -1,49 +1,39 @@
 package it.unipv.java.view;
 
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
-import com.jgoodies.forms.factories.DefaultComponentFactory;
+
 import java.awt.Color;
-import javax.swing.JTextPane;
-import javax.swing.JRadioButtonMenuItem;
+
 import javax.swing.JSeparator;
 import java.awt.Toolkit;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
-import java.awt.Rectangle;
-import javax.swing.border.BevelBorder;
-import javax.swing.border.CompoundBorder;
 import javax.swing.border.EtchedBorder;
-import java.awt.Canvas;
 import java.awt.Dimension;
-import java.awt.Window.Type;
+import java.awt.event.ActionListener;
 import java.awt.SystemColor;
-import javax.swing.JTextField;
 import javax.swing.JTextArea;
 import javax.swing.JPasswordField;
 
 public class LoginView extends JFrame {
 
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JPasswordField passwordField;
-	
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					LoginView frame = new LoginView();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private JPasswordField textAreaPassword;
+	private JPanel panelLogin;
+	private JLabel lblNewLabel;
+	private JSeparator separator;
+	private JLabel emailLabel;
+	private JLabel passwordLabel;
+	private JSeparator separator_1;
+	private JButton loginButton;
+	private JTextArea textAreaEmail;
+	private JPanel panel;
+	private JLabel lblNewLabel_2;
 
 	public LoginView() {
 		setResizable(false);
@@ -58,64 +48,64 @@ public class LoginView extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JPanel panelLogin = new JPanel();
+		panelLogin = new JPanel();
 		panelLogin.setBackground(Color.WHITE);
 		panelLogin.setForeground(Color.WHITE);
 		panelLogin.setBounds(0, 0, 222, 261);
 		contentPane.add(panelLogin);
 		panelLogin.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Sign In");
+		lblNewLabel = new JLabel("Sign In");
 		lblNewLabel.setForeground(new Color(0, 128, 255));
 		lblNewLabel.setFont(new Font("Segoe UI Black", Font.PLAIN, 18));
 		lblNewLabel.setBounds(10, 0, 72, 52);
 		panelLogin.add(lblNewLabel);
 		
-		JSeparator separator = new JSeparator();
+		separator = new JSeparator();
 		separator.setBounds(10, 99, 191, 2);
 		panelLogin.add(separator);
 		
-		JLabel lblNewLabel_1 = new JLabel("Email");
-		lblNewLabel_1.setForeground(Color.GRAY);
-		lblNewLabel_1.setFont(new Font("Yu Gothic Light", Font.PLAIN, 11));
-		lblNewLabel_1.setBounds(10, 63, 46, 14);
-		panelLogin.add(lblNewLabel_1);
+		emailLabel = new JLabel("Email");
+		emailLabel.setForeground(Color.GRAY);
+		emailLabel.setFont(new Font("Yu Gothic Light", Font.PLAIN, 11));
+		emailLabel.setBounds(10, 63, 46, 14);
+		panelLogin.add(emailLabel);
 		
-		JLabel lblNewLabel_1_1 = new JLabel("Password");
-		lblNewLabel_1_1.setForeground(Color.GRAY);
-		lblNewLabel_1_1.setFont(new Font("Yu Gothic Light", Font.PLAIN, 11));
-		lblNewLabel_1_1.setBounds(10, 137, 60, 14);
-		panelLogin.add(lblNewLabel_1_1);
+		passwordLabel = new JLabel("Password");
+		passwordLabel.setForeground(Color.GRAY);
+		passwordLabel.setFont(new Font("Yu Gothic Light", Font.PLAIN, 11));
+		passwordLabel.setBounds(10, 137, 60, 14);
+		panelLogin.add(passwordLabel);
 		
-		JSeparator separator_1 = new JSeparator();
+		separator_1 = new JSeparator();
 		separator_1.setBounds(10, 171, 191, 14);
 		panelLogin.add(separator_1);
 		
-		JButton btnNewButton = new JButton("Login");
-		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnNewButton.setBorderPainted(false);
-		btnNewButton.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-		btnNewButton.setIcon(null);
-		btnNewButton.setForeground(Color.WHITE);
-		btnNewButton.setBackground(new Color(0, 128, 255));
-		btnNewButton.setBounds(21, 208, 162, 28);
-		panelLogin.add(btnNewButton);
+		loginButton = new JButton("Login");
+		loginButton.setFont(new Font("Tahoma", Font.BOLD, 11));
+		loginButton.setBorderPainted(false);
+		loginButton.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		loginButton.setIcon(null);
+		loginButton.setForeground(Color.WHITE);
+		loginButton.setBackground(new Color(0, 128, 255));
+		loginButton.setBounds(21, 208, 162, 28);
+		panelLogin.add(loginButton);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(10, 77, 191, 22);
-		panelLogin.add(textArea);
+		textAreaEmail = new JTextArea();
+		textAreaEmail.setBounds(10, 77, 191, 22);
+		panelLogin.add(textAreaEmail);
 		
-		passwordField = new JPasswordField();
-		passwordField.setBorder(null);
-		passwordField.setBounds(10, 150, 191, 20);
-		panelLogin.add(passwordField);
+		textAreaPassword = new JPasswordField();
+		textAreaPassword.setBorder(null);
+		textAreaPassword.setBounds(10, 150, 191, 20);
+		panelLogin.add(textAreaPassword);
 		
-		JPanel panel = new JPanel();
+		panel = new JPanel();
 		panel.setBounds(220, 0, 222, 261);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
-		JLabel lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2 = new JLabel("");
 		lblNewLabel_2.setIconTextGap(0);
 		lblNewLabel_2.setMaximumSize(new Dimension(6, 5));
 		lblNewLabel_2.setSize(new Dimension(4, 4));
@@ -123,4 +113,20 @@ public class LoginView extends JFrame {
 		lblNewLabel_2.setBounds(-16, 0, 238, 261);
 		panel.add(lblNewLabel_2);
 	}
+	
+	public String getEmail() {
+		return textAreaEmail.getText();
+	}
+	
+	public String getPassword() {
+		return textAreaPassword.getSelectedText();
+	}
+	
+	void addLoginListener(ActionListener listenForLogin) {
+		loginButton.addActionListener(listenForLogin);
+	}
+	
+	
+	
+	
 }
