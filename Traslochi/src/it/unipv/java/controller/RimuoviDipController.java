f028d0f8c5024c819c8a3b9795b00fac3cc0bac9
NikoManev <nikola.manev01@universitadipavia.it>
2024-02-12 15:01:47 +0100

    controller

--- Traslochi/src/it/unipv/java/controller/RimuoviDipController.java
+++ Traslochi/src/it/unipv/java/controller/RimuoviDipController.java
@@ -1,5 +1,10 @@
 package it.unipv.java.controller;
 
+import it.unipv.java.view.RimuoviDipView;
+
 public class RimuoviDipController {
 
+	private RimuoviDipView rdv;
+	
+	
 }
--- Traslochi/src/it/unipv/java/model/DipendenteModel.java
+++ Traslochi/src/it/unipv/java/model/DipendenteModel.java
@@ -1,26 +1,41 @@
 package it.unipv.java.model;
- 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DipendenteModel extends UserModel {
-    private int idDipendente;
+    private String idDipendente;
     private String mezzo;
 
     // Costruttore di default che chiama il costruttore della superclasse
-    public DipendenteModel() {
-     }
+    /*public DipendenteModel() {
+     }*/
     
     // Costruttore parametrizzato per inizializzare un Dipendente con tutti gli attributi
-    public DipendenteModel(String nome, String cognome,String cf, String email, String password, UserType userType, int idDipendente, String mezzo) {
+    public DipendenteModel(String nome, String cognome,String cf, String email, String password, UserType userType, String idDipendente) {
         super(nome, cognome,cf, email, password, userType);
         this.idDipendente = idDipendente;
         this.mezzo = mezzo;
     }
-
+    
+    /*public List<DipendenteModel> getListaDipendenti() {
+        // Crea una nuova lista per memorizzare i dipendenti
+        List<DipendenteModel> dipendenti = new ArrayList<>();
+        
+        // Aggiungi i dipendenti alla lista
+        /*dipendenti.add(new DipendenteModel("Mario", "Rossi", "ABCD1234", "mario@example.com", "password123", UserType.DIPENDENTE, "1"));
+        dipendenti.add(new DipendenteModel("Luigi", "Verdi", "EFGH5678", "luigi@example.com", "password456", UserType.DIPENDENTE, "2"));
+        // Aggiungi altri dipendenti come necessario*/
+        
+       /* return dipendenti;
+    }*/
+    
     // Getter e setter per idDipendente
-    public int getIdDipendente() {
+    public String getIdDipendente() {
         return idDipendente;
     }
 
-    public void setIdDipendente(int idDipendente) {
+    public void setIdDipendente(String idDipendente) {
         this.idDipendente = idDipendente;
     }
 
