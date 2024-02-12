cfc68d04d00826effe20b31600603d005a9d9fe0
NikoManev <nikola.manev01@universitadipavia.it>
2024-02-13 00:13:37 +0100

    new DipendenteView

--- Traslochi/src/it/unipv/java/view/DipendenteView.java
+++ Traslochi/src/it/unipv/java/view/DipendenteView.java
@@ -4,26 +4,21 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
- import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import java.awt.Color;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.Color;
 import javax.swing.JButton;
 import java.awt.SystemColor;
-import java.awt.Toolkit;
-import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JTextPane;
-import javax.swing.ImageIcon;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
 public class DipendenteView extends JFrame {
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+
 	private JPanel contentPane;
-	private JButton buttonVisProfilo;
+	private JButton buttonVisProfilo_1;
 	private JTextPane orarioInizioTurno;
 	private JTextPane orarioFineTurno;
 	private JTextPane indirizzoLavoro;
@@ -43,80 +38,77 @@ public class DipendenteView extends JFrame {
 			}
 		});
 	}
- 
+
+	
 	public DipendenteView() {
-		setIconImage(Toolkit.getDefaultToolkit().getImage("resources/IconaLogin.png"));
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\nikola.manev\\Desktop\\immagini java\\bookPren.jpg"));
 		setTitle("Bidons");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1421, 682);
+		setBounds(100, 100, 917, 505);
 		contentPane = new JPanel();
-		contentPane.setBackground(SystemColor.textHighlightText);
+		contentPane.setBackground(SystemColor.window);
 		contentPane.setBorder(null);
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBorder(null);
 		panel.setBackground(new Color(30, 144, 255));
+		panel.setBorder(null);
+		panel.setBounds(0, 0, 1206, 79);
+		contentPane.add(panel);
 		
-		JLabel lblTurniLavorativi = new JLabel("Turno Lavorativo");
-		lblTurniLavorativi.setForeground(new Color(248, 248, 255));
-		lblTurniLavorativi.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		JLabel lblNewLabel = new JLabel("Turno Lavorativo");
+		lblNewLabel.setForeground(new Color(248, 248, 255));
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
 		
-		buttonVisProfilo = new JButton("Visualizza Profilo");
-		buttonVisProfilo.setForeground(SystemColor.window);
-		buttonVisProfilo.setFont(new Font("Tahoma", Font.BOLD, 11));
-		buttonVisProfilo.setFocusable(false);
-		buttonVisProfilo.setBorder(null);
-		buttonVisProfilo.setBackground(SystemColor.textHighlight);
+		buttonVisProfilo_1 = new JButton("Visualizza Profilo");
+		buttonVisProfilo_1.setForeground(SystemColor.window);
+		buttonVisProfilo_1.setFont(new Font("Tahoma", Font.BOLD, 11));
+		buttonVisProfilo_1.setFocusable(false);
+		buttonVisProfilo_1.setBorder(null);
+		buttonVisProfilo_1.setBackground(SystemColor.textHighlight);
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel.createSequentialGroup()
-					.addGap(83)
-					.addComponent(lblTurniLavorativi, GroupLayout.DEFAULT_SIZE, 225, Short.MAX_VALUE)
-					.addGap(728)
-					.addComponent(buttonVisProfilo, GroupLayout.PREFERRED_SIZE, 119, GroupLayout.PREFERRED_SIZE)
-					.addGap(221))
+					.addGap(54)
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 205, GroupLayout.PREFERRED_SIZE)
+					.addGap(431)
+					.addComponent(buttonVisProfilo_1, GroupLayout.DEFAULT_SIZE, 119, Short.MAX_VALUE)
+					.addGap(397))
 		);
 		gl_panel.setVerticalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblTurniLavorativi, GroupLayout.DEFAULT_SIZE, 45, Short.MAX_VALUE)
-						.addComponent(buttonVisProfilo, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE))
-					.addGap(19))
+					.addGap(11)
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 57, GroupLayout.PREFERRED_SIZE))
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(21)
+					.addComponent(buttonVisProfilo_1, GroupLayout.DEFAULT_SIZE, 32, Short.MAX_VALUE)
+					.addGap(26))
 		);
 		panel.setLayout(gl_panel);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(SystemColor.text);
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addComponent(panel, GroupLayout.PREFERRED_SIZE, 1376, GroupLayout.PREFERRED_SIZE)
-				.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 1261, GroupLayout.PREFERRED_SIZE)
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 75, Short.MAX_VALUE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 384, GroupLayout.PREFERRED_SIZE)
-					.addGap(78))
-		);
 		
-		JLabel lblNewLabel = new JLabel("Orario di Inizio Turno");
-		lblNewLabel.setForeground(SystemColor.activeCaption);
-		lblNewLabel.setBackground(SystemColor.activeCaption);
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(SystemColor.window);
+		panel_1.setBorder(null);
+		panel_1.setBounds(0, 78, 901, 388);
+		contentPane.add(panel_1);
+		
+		JLabel lblNewLabel_1 = new JLabel("Orario di Inizio Turno");
+		lblNewLabel_1.setForeground(SystemColor.activeCaption);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_1.setBackground(SystemColor.activeCaption);
 		
 		JLabel lblOrarioDiFine = new JLabel("Orario di Fine Turno\r\n");
 		lblOrarioDiFine.setForeground(SystemColor.activeCaption);
 		lblOrarioDiFine.setFont(new Font("Tahoma", Font.BOLD, 15));
 		
 		orarioInizioTurno = new JTextPane();
+		orarioInizioTurno.setEditable(false);
 		orarioInizioTurno.setBackground(SystemColor.inactiveCaptionBorder);
 		
 		orarioFineTurno = new JTextPane();
@@ -127,110 +119,85 @@ public class DipendenteView extends JFrame {
 		lblLuogoDiLavoro.setFont(new Font("Tahoma", Font.BOLD, 15));
 		
 		indirizzoLavoro = new JTextPane();
+		indirizzoLavoro.setEditable(false);
 		indirizzoLavoro.setBackground(SystemColor.inactiveCaptionBorder);
 		
 		buttonInizioTurno = new JButton("Inizio Turno");
 		buttonInizioTurno.setForeground(SystemColor.text);
 		buttonInizioTurno.setFont(new Font("Tahoma", Font.BOLD, 13));
-		buttonInizioTurno.setBackground(SystemColor.textHighlight);
-		buttonInizioTurno.setBorder(null);
 		buttonInizioTurno.setFocusable(false);
+		buttonInizioTurno.setBorder(null);
+		buttonInizioTurno.setBackground(SystemColor.textHighlight);
 		
 		buttonFineTurno = new JButton("Fine Turno");
 		buttonFineTurno.setForeground(SystemColor.text);
 		buttonFineTurno.setFont(new Font("Tahoma", Font.BOLD, 13));
-		buttonFineTurno.setBackground(SystemColor.textHighlight);
-		buttonFineTurno.setBorder(null);
 		buttonFineTurno.setFocusable(false);
-		
-		JLabel lblNewLabel_1 = new JLabel("");
-		lblNewLabel_1.setIcon(new ImageIcon("resources/icona tempo.jpg"));
-		
-		JLabel lblNewLabel_1_1 = new JLabel("");
-		lblNewLabel_1_1.setIcon(new ImageIcon("resources/icona tempo.jpg"));
-		
-		JLabel lblNewLabel_1_2 = new JLabel("");
-		lblNewLabel_1_2.setIcon(new ImageIcon("resources/iconaIndirizzo.jpg"));
+		buttonFineTurno.setBorder(null);
+		buttonFineTurno.setBackground(SystemColor.textHighlight);
 		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
 		gl_panel_1.setHorizontalGroup(
 			gl_panel_1.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_1.createSequentialGroup()
+					.addGap(129)
 					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel_1.createSequentialGroup()
-							.addGap(189)
-							.addComponent(lblNewLabel_1)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, 231, Short.MAX_VALUE)
-							.addGap(596)
-							.addComponent(lblNewLabel_1_1))
-						.addGroup(gl_panel_1.createSequentialGroup()
-							.addGap(232)
-							.addComponent(orarioInizioTurno, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE)))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_panel_1.createParallelGroup(Alignment.TRAILING)
+						.addComponent(lblNewLabel_1, GroupLayout.DEFAULT_SIZE, 173, Short.MAX_VALUE)
 						.addGroup(gl_panel_1.createSequentialGroup()
 							.addGap(28)
-							.addComponent(orarioFineTurno, GroupLayout.DEFAULT_SIZE, 87, Short.MAX_VALUE)
-							.addGap(284))
-						.addGroup(gl_panel_1.createSequentialGroup()
-							.addComponent(lblOrarioDiFine, GroupLayout.DEFAULT_SIZE, 167, Short.MAX_VALUE)
-							.addGap(232))))
-				.addGroup(gl_panel_1.createSequentialGroup()
-					.addGap(469)
-					.addComponent(buttonInizioTurno, GroupLayout.DEFAULT_SIZE, 90, Short.MAX_VALUE)
-					.addGap(190)
-					.addComponent(buttonFineTurno, GroupLayout.DEFAULT_SIZE, 82, Short.MAX_VALUE)
-					.addGap(600))
-				.addGroup(gl_panel_1.createSequentialGroup()
-					.addGap(479)
-					.addComponent(lblNewLabel_1_2)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(lblLuogoDiLavoro)
-					.addContainerGap(629, Short.MAX_VALUE))
-				.addGroup(gl_panel_1.createSequentialGroup()
-					.addGap(347)
-					.addComponent(indirizzoLavoro, GroupLayout.DEFAULT_SIZE, 668, Short.MAX_VALUE)
-					.addGap(416))
-		);
-		gl_panel_1.setVerticalGroup(
-			gl_panel_1.createParallelGroup(Alignment.TRAILING)
-				.addGroup(gl_panel_1.createSequentialGroup()
-					.addGroup(gl_panel_1.createParallelGroup(Alignment.TRAILING)
-						.addGroup(gl_panel_1.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(lblNewLabel_1))
-						.addGroup(gl_panel_1.createParallelGroup(Alignment.TRAILING)
-							.addGroup(gl_panel_1.createSequentialGroup()
-								.addGap(26)
-								.addComponent(lblNewLabel_1_1))
-							.addGroup(gl_panel_1.createSequentialGroup()
-								.addGap(20)
-								.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE)
-									.addComponent(lblOrarioDiFine, GroupLayout.DEFAULT_SIZE, 32, Short.MAX_VALUE)
-									.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, 32, Short.MAX_VALUE)))))
-					.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(orarioInizioTurno, GroupLayout.DEFAULT_SIZE, 90, Short.MAX_VALUE)
+							.addGap(55)))
+					.addGap(311)
 					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_panel_1.createSequentialGroup()
-							.addGroup(gl_panel_1.createParallelGroup(Alignment.TRAILING)
-								.addGroup(gl_panel_1.createSequentialGroup()
-									.addComponent(orarioFineTurno, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-									.addGap(36)
-									.addComponent(lblNewLabel_1_2, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE))
-								.addComponent(lblLuogoDiLavoro, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE))
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(indirizzoLavoro, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addGap(130)
-							.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING, false)
-								.addComponent(buttonFineTurno, GroupLayout.DEFAULT_SIZE, 49, Short.MAX_VALUE)
-								.addComponent(buttonInizioTurno, GroupLayout.DEFAULT_SIZE, 49, Short.MAX_VALUE)))
-						.addComponent(orarioInizioTurno, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(149))
+							.addGap(31)
+							.addComponent(orarioFineTurno, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE))
+						.addComponent(lblOrarioDiFine, GroupLayout.DEFAULT_SIZE, 163, Short.MAX_VALUE))
+					.addGap(125))
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addGap(274)
+					.addComponent(lblLuogoDiLavoro, GroupLayout.DEFAULT_SIZE, 325, Short.MAX_VALUE)
+					.addGap(302))
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addGap(187)
+					.addComponent(indirizzoLavoro, GroupLayout.DEFAULT_SIZE, 503, Short.MAX_VALUE)
+					.addGap(211))
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addGap(278)
+					.addComponent(buttonInizioTurno, GroupLayout.DEFAULT_SIZE, 90, Short.MAX_VALUE)
+					.addGap(164)
+					.addComponent(buttonFineTurno, GroupLayout.DEFAULT_SIZE, 80, Short.MAX_VALUE)
+					.addGap(289))
+		);
+		gl_panel_1.setVerticalGroup(
+			gl_panel_1.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_1.createSequentialGroup()
+					.addGap(26)
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel_1.createSequentialGroup()
+							.addComponent(lblNewLabel_1, GroupLayout.DEFAULT_SIZE, 32, Short.MAX_VALUE)
+							.addGap(19))
+						.addGroup(gl_panel_1.createSequentialGroup()
+							.addGap(31)
+							.addComponent(orarioInizioTurno))
+						.addGroup(gl_panel_1.createSequentialGroup()
+							.addGap(31)
+							.addComponent(orarioFineTurno, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel_1.createSequentialGroup()
+							.addComponent(lblOrarioDiFine, GroupLayout.DEFAULT_SIZE, 32, Short.MAX_VALUE)
+							.addGap(19)))
+					.addGap(48)
+					.addComponent(lblLuogoDiLavoro, GroupLayout.DEFAULT_SIZE, 26, Short.MAX_VALUE)
+					.addGap(13)
+					.addComponent(indirizzoLavoro, GroupLayout.DEFAULT_SIZE, 20, Short.MAX_VALUE)
+					.addGap(94)
+					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+						.addComponent(buttonInizioTurno, GroupLayout.PREFERRED_SIZE, 49, GroupLayout.PREFERRED_SIZE)
+						.addComponent(buttonFineTurno, GroupLayout.DEFAULT_SIZE, 49, Short.MAX_VALUE))
+					.addGap(61))
 		);
 		panel_1.setLayout(gl_panel_1);
-		contentPane.setLayout(gl_contentPane);
 	}
-
-
+	
 	public JPanel getContentPane() {
 		return contentPane;
 	}
@@ -242,12 +209,12 @@ public class DipendenteView extends JFrame {
 
 
 	public JButton getButtonVisProfilo() {
-		return buttonVisProfilo;
+		return buttonVisProfilo_1;
 	}
 
 
 	public void setButtonVisProfilo(JButton buttonVisProfilo) {
-		this.buttonVisProfilo = buttonVisProfilo;
+		this.buttonVisProfilo_1 = buttonVisProfilo;
 	}
 
 
