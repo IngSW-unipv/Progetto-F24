5d64fae37579540f3992bdaf0d65940026e67d20
NikoManev <nikola.manev01@universitadipavia.it>
2024-02-28 10:57:44 +0100

    update

--- Traslochi/src/it/unipv/java/view/RimuoviDipView.java
+++ Traslochi/src/it/unipv/java/view/RimuoviDipView.java
@@ -16,6 +16,7 @@ import javax.swing.JSeparator;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JTextField;
 import java.awt.Toolkit;
+import javax.swing.SwingConstants;
 
 public class RimuoviDipView extends JDialog {
 
@@ -23,7 +24,7 @@ public class RimuoviDipView extends JDialog {
 	private JButton buttonConfermaRim;
 	private JTextField idDipendente;
 	private JTextField codiceFiscale;
-	private JButton buttonReturn;
+	private JButton buttonAnnulla;
 
 	
 	
@@ -47,6 +48,8 @@ public class RimuoviDipView extends JDialog {
 		contentPanel.setBackground(SystemColor.inactiveCaptionBorder);
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		setLocationRelativeTo(null);
+		
 		JTextPane txtpnPerRimuovereUn = new JTextPane();
 		txtpnPerRimuovereUn.setEditable(false);
 		txtpnPerRimuovereUn.setBackground(SystemColor.inactiveCaptionBorder);
@@ -54,9 +57,11 @@ public class RimuoviDipView extends JDialog {
 		txtpnPerRimuovereUn.setFont(new Font("Tahoma", Font.BOLD, 13));
 		txtpnPerRimuovereUn.setText("Per rimuovere un Dipendente inserire");
 		JLabel lblNewLabel = new JLabel("ID Dipendente");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setForeground(SystemColor.activeCaption);
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
 		JLabel lblNewLabel_1 = new JLabel("Codice Fiscale");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_1.setForeground(SystemColor.activeCaption);
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 11));
 		JSeparator separator = new JSeparator();
@@ -73,61 +78,40 @@ public class RimuoviDipView extends JDialog {
 		codiceFiscale.setBackground(SystemColor.inactiveCaptionBorder);
 		codiceFiscale.setBorder(null);
 		codiceFiscale.setColumns(10);
-		
-		buttonReturn = new JButton("<");
-		buttonReturn.setForeground(SystemColor.window);
-		buttonReturn.setBackground(SystemColor.textHighlight);
-		buttonReturn.setBorder(null);
-		buttonReturn.setFont(new Font("Tahoma", Font.BOLD, 8));
-		buttonReturn.setFocusable(false);
 		GroupLayout gl_contentPanel = new GroupLayout(contentPanel);
 		gl_contentPanel.setHorizontalGroup(
 			gl_contentPanel.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_contentPanel.createSequentialGroup()
-					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPanel.createSequentialGroup()
-							.addGap(6)
-							.addComponent(buttonReturn, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(txtpnPerRimuovereUn, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_contentPanel.createSequentialGroup()
-							.addGap(144)
-							.addGroup(gl_contentPanel.createParallelGroup(Alignment.TRAILING)
-								.addComponent(lblNewLabel)
-								.addComponent(lblNewLabel_1))))
+					.addGap(54)
+					.addComponent(txtpnPerRimuovereUn, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap(61, Short.MAX_VALUE))
 				.addGroup(gl_contentPanel.createSequentialGroup()
-					.addGroup(gl_contentPanel.createParallelGroup(Alignment.TRAILING)
-						.addGroup(gl_contentPanel.createSequentialGroup()
-							.addGap(77)
-							.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
-								.addComponent(idDipendente, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 219, Short.MAX_VALUE)
-								.addComponent(separator_1, GroupLayout.DEFAULT_SIZE, 219, Short.MAX_VALUE)))
-						.addGroup(gl_contentPanel.createSequentialGroup()
-							.addContainerGap(77, Short.MAX_VALUE)
-							.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
-								.addComponent(separator, GroupLayout.PREFERRED_SIZE, 219, GroupLayout.PREFERRED_SIZE)
-								.addComponent(codiceFiscale, GroupLayout.PREFERRED_SIZE, 219, GroupLayout.PREFERRED_SIZE))))
+					.addContainerGap(64, Short.MAX_VALUE)
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING, false)
+						.addComponent(lblNewLabel, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(lblNewLabel_1, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(codiceFiscale, Alignment.TRAILING)
+						.addComponent(idDipendente, Alignment.TRAILING)
+						.addComponent(separator_1, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 232, Short.MAX_VALUE)
+						.addComponent(separator, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 232, Short.MAX_VALUE))
 					.addGap(69))
 		);
 		gl_contentPanel.setVerticalGroup(
 			gl_contentPanel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPanel.createSequentialGroup()
-					.addGroup(gl_contentPanel.createParallelGroup(Alignment.TRAILING, false)
-						.addComponent(buttonReturn, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(txtpnPerRimuovereUn, Alignment.LEADING))
+					.addComponent(txtpnPerRimuovereUn, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addGap(18)
 					.addComponent(lblNewLabel)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(idDipendente, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(separator_1, GroupLayout.PREFERRED_SIZE, 2, GroupLayout.PREFERRED_SIZE)
+					.addComponent(separator_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addGap(24)
 					.addComponent(lblNewLabel_1)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(codiceFiscale, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(separator, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addComponent(separator, GroupLayout.PREFERRED_SIZE, 2, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap(22, Short.MAX_VALUE))
 		);
 		contentPanel.setLayout(gl_contentPanel);
@@ -145,19 +129,31 @@ public class RimuoviDipView extends JDialog {
 				getRootPane().setDefaultButton(buttonConfermaRim);
 				buttonConfermaRim.setFocusable(false);
 			}
+			
+			buttonAnnulla = new JButton("Annulla");
+			buttonAnnulla.setForeground(SystemColor.window);
+			buttonAnnulla.setFont(new Font("Tahoma", Font.BOLD, 11));
+			buttonAnnulla.setFocusable(false);
+			buttonAnnulla.setBorder(null);
+			buttonAnnulla.setBackground(SystemColor.textHighlight);
+			buttonAnnulla.setActionCommand("OK");
 			GroupLayout gl_buttonPane = new GroupLayout(buttonPane);
 			gl_buttonPane.setHorizontalGroup(
 				gl_buttonPane.createParallelGroup(Alignment.LEADING)
 					.addGroup(gl_buttonPane.createSequentialGroup()
-						.addGap(146)
+						.addGap(79)
 						.addComponent(buttonConfermaRim, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
-						.addContainerGap(154, Short.MAX_VALUE))
+						.addGap(63)
+						.addComponent(buttonAnnulla, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(83, Short.MAX_VALUE))
 			);
 			gl_buttonPane.setVerticalGroup(
-				gl_buttonPane.createParallelGroup(Alignment.TRAILING)
-					.addGroup(Alignment.LEADING, gl_buttonPane.createSequentialGroup()
+				gl_buttonPane.createParallelGroup(Alignment.LEADING)
+					.addGroup(gl_buttonPane.createSequentialGroup()
 						.addContainerGap()
-						.addComponent(buttonConfermaRim, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_buttonPane.createParallelGroup(Alignment.BASELINE)
+							.addComponent(buttonAnnulla, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE)
+							.addComponent(buttonConfermaRim, GroupLayout.PREFERRED_SIZE, 15, GroupLayout.PREFERRED_SIZE))
 						.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 			);
 			buttonPane.setLayout(gl_buttonPane);
@@ -180,8 +176,8 @@ public class RimuoviDipView extends JDialog {
 
 
 	
-	public JButton getButtonReturn() {
-		return buttonReturn;
+	public JButton getButtonAnnulla() {
+		return buttonAnnulla;
 	}
 
 
@@ -207,5 +203,4 @@ public class RimuoviDipView extends JDialog {
 	public void setCodiceFiscale(JTextField codiceFiscale) {
 		this.codiceFiscale = codiceFiscale;
 	}
-
 }
