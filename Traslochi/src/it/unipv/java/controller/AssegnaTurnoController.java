27a746902a76acab71f6c8a2cebb5e6dd1fc614e
NikoManev <nikola.manev01@universitadipavia.it>
2024-02-28 20:38:08 +0100

    update

--- Traslochi/src/it/unipv/java/controller/AssegnaTurnoController.java
+++ Traslochi/src/it/unipv/java/controller/AssegnaTurnoController.java
@@ -1,5 +1,49 @@
 package it.unipv.java.controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import it.unipv.java.model.TurnoModel;
+import it.unipv.java.persistance.PersistanceFacade;
+import it.unipv.java.view.AssegnaTurnoView;
+
 public class AssegnaTurnoController {
 
+	private AssegnaTurnoView atv;
+	private TurnoModel tm;
+	
+	public AssegnaTurnoController(AssegnaTurnoView atv) {
+		this.atv = atv;
+		setListeners();
+	}
+	
+	private void setListeners() {
+		atv.getButtonAnnulla().addActionListener(new ActionListener() {	
+			public void actionPerformed(ActionEvent e) {
+				atv.setVisible(false);	
+			}
+		});
+		
+		atv.getButtonConfTurno().addActionListener(new ActionListener() {	
+			public void actionPerformed(ActionEvent e) {
+				tm = new TurnoModel();
+				tm.setIdDipendente(atv.getIdDip());
+				tm.setIndLavoro(atv.getIndLavoro());
+				tm.setOrarioini(atv.getOrarioIniTur());
+				
+				boolean result = PersistanceFacade.getInstance().aggiungiTurno(tm);
+				if(result) {
+					
+				}
+			}
+		});
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 }
